38051747e777d170208ce6069cd71267e4b67be6
Stepan Vasilyev <intellectualspecialist@gmail.com>
2026-02-03 19:06:45 +0500

    feat: добавляет сортировку + фиксит код

--- src/view/sort-view/sort-template.js
+++ src/view/sort-view/sort-template.js
@@ -1,12 +1,14 @@
 import { capitalizeString } from '../../utils/common';
+import { SortingType } from '../../const';
 
 function createSortItemTemplate({type, points}) {
   const capitalizedType = capitalizeString(type);
   const disabled = points ? '' : 'disabled';
+  const checked = type === SortingType.DAY ? 'checked' : '';
 
   return `
     <div class="trip-sort__item  trip-sort__item--${type}">
-      <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" ${disabled}>
+      <input id="sort-${type}" data-sort-type="${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" ${checked} ${disabled}>
       <label class="trip-sort__btn" for="sort-${type}">${capitalizedType}</label>
     </div>
   `;
--- src/view/sort-view/sort-view.js
+++ src/view/sort-view/sort-view.js
@@ -3,13 +3,29 @@ import AbstractView from '../../framework/view/abstract-view';
 
 export default class SortView extends AbstractView {
   #sortingItems = [];
+  #handleSortChange = null;
 
-  constructor({sortingItems}) {
+  constructor({sortingItems, onSortChange}) {
     super();
     this.#sortingItems = sortingItems;
+    this.#handleSortChange = onSortChange;
+
+    this.#initEventListeners();
   }
 
   get template() {
     return createSortTemplate(this.#sortingItems);
   }
+
+  #initEventListeners() {
+    this.element.addEventListener('change', this.#sortChangeHandler);
+  }
+
+  #sortChangeHandler = (evt) => {
+    evt.preventDefault();
+    const currentValue = this.element['trip-sort'].value;
+    const currentType = currentValue.replace('sort-', '');
+
+    this.#handleSortChange(currentType);
+  };
 }
